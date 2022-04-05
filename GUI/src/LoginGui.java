19a8014120f243a0fd7364533a95cbc6a7117cab
ikenna6 <ogbuerii@bzz.ch>
2022-04-05 11:12:41 +0200

    started with tuto and finished LoginGui

--- GUI/src/LoginGui.java
+++ GUI/src/LoginGui.java
@@ -1,2 +1,87 @@
-public class LoginGui {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginGui extends JFrame {
+
+    //Gui elemente erzeugen
+
+    private String username = "ikenna";
+    private String passwort = "leo";
+
+    private static int anzVersuche = 3;
+    JButton btnLogin = new JButton("login");
+
+    JLabel labelUsername = new JLabel("Benutzername                       ");
+    JLabel labelPwd = new JLabel("Passwort");
+    JLabel labelHinweis = new JLabel("Sie haben noch " + anzVersuche + " Versuche!");
+
+    JTextField eingabeUser = new JTextField();
+    JTextField eingabePwd = new JTextField();
+
+    JPanel panelUsername = new JPanel();
+    JPanel panelPwd = new JPanel();
+    JPanel panelTest = new JPanel();
+    JPanel panelTest2 = new JPanel();
+
+    //JPanel panelLoginButton = new JPanel();
+
+
+    //panels erzeugen
+    public LoginGui() {
+        //Frame gestalten
+        super("Login");
+        setResizable(false);
+        setSize(150,400);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new BorderLayout(15,15));
+        panelUsername.setLayout(new GridLayout(2, 1));
+        panelPwd.setLayout(new GridLayout(3, 1));
+        panelTest.setLayout(new BorderLayout(15,15));
+        panelTest2.setLayout(new BorderLayout(15,15));
+
+        //Panel zusammensetzten
+        panelUsername.add(labelUsername);
+        panelUsername.add(eingabeUser);
+
+        panelTest2.add(panelUsername, BorderLayout.WEST);
+
+        panelPwd.add(labelPwd);
+        panelPwd.add(eingabePwd);
+        panelPwd.add(labelHinweis);
+
+        panelTest.add(panelPwd, BorderLayout.WEST);
+
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (eingabeUser.getText().equals(username) && eingabePwd.getText().equals(passwort)){
+                    LoginGui.super.dispose();
+                    new MainGui();
+                }else {
+                    if (anzVersuche <=1){
+                        LoginGui.super.dispose();
+                    }
+                    anzVersuche--;
+                    eingabePwd.setText("");
+                    System.out.println(" Versuche sind jetzt runter" + anzVersuche);
+                    labelHinweis.setText("Sie haben noch " + anzVersuche + " Versuche!");
+                }
+            }
+        });
+        //Gui zusammensetzten
+        getContentPane().add(panelTest2, BorderLayout.NORTH);
+        getContentPane().add(panelTest, BorderLayout.CENTER);
+        getContentPane().add(btnLogin, BorderLayout.SOUTH);
+
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new LoginGui();
+    }
 }
+
--- GUI/src/TutorialGui.java
+++ GUI/src/TutorialGui.java
@@ -1,22 +1,87 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class TutorialGui extends JFrame {
+    private int  page = 0;
+    ArrayList<JTextArea> seiten = new ArrayList<>();
+    JTextArea text0 = new JTextArea("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+    JTextArea text1 = new JTextArea("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. " +
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,");
+
+
+    JTextArea text2 = new JTextArea("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ");
+    JTextArea text3 = new JTextArea("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l");
+    JTextArea text4 = new JTextArea("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+    JTextArea text5 = new JTextArea("ENde des Tutorials");
+
+
+    JButton arrowRight = new JButton("right");
+    JButton arrowLeft = new JButton("left");
+
+    JPanel btnPanel = new JPanel();
+    JPanel lowerPanel = new JPanel();
+
+
+    JCheckBox checkBox = new JCheckBox("Tutorial nicht mehr anzeigen");
 
     public TutorialGui() {
         super("TutorialGUI");
+        seiten.add(text0);
+        seiten.add(text1);
+        seiten.add(text2);
+        seiten.add(text3);
+        seiten.add(text4);
+        seiten.add(text5);
         init();
         this.pack();
         this.setVisible(true);
     }
 
     private void init() {
-        //textArea
-        JTextArea textArea = new JTextArea("Tutorial");
-        textArea.setRows(20); //Anzahl Zeilen
-        textArea.setColumns(50); //Anzahl Zeichen auf der Zeile
 
-        setLayout(new BorderLayout());
-        this.add(textArea, BorderLayout.CENTER);
+        btnPanel.setLayout(new GridLayout(1, 2));
+        lowerPanel.setLayout(new BorderLayout(15,15));
+
+        text0.setLineWrap(true);
+        text1.setLineWrap(true);
+        text2.setLineWrap(true);
+
+        btnPanel.add(arrowLeft);
+        btnPanel.add(arrowRight);
+        arrowLeft.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (page == 0){
+                    setEnabled(false);
+                }else {
+                    page--;
+                }
+            }
+        });
+
+        arrowRight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (page == 5){
+                    setEnabled(false);
+                }else {
+                    page++;
+                }
+            }
+        });
+
+
+        lowerPanel.add(btnPanel, BorderLayout.EAST);
+        lowerPanel.add(checkBox, BorderLayout.WEST);
+
+        getContentPane().add(text1);
+        getContentPane().add(lowerPanel, BorderLayout.SOUTH);
+    }
+
+    public static void main(String[] args) {
+        new TutorialGui();
     }
 }
